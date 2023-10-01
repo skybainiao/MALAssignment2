2d815497f74bfd1632e06a6eec9988da864d7d10
Junjie Chen <chen2468830035@gmail.com>
2023-10-02 00:58:20 +0200

    a

--- main.py
+++ main.py
@@ -1,61 +1,75 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
 
 
+file_path = 'C:/Users/45527/Downloads/exoplanet_dataset.csv'
+df = pd.read_csv(file_path)
 
-df = pd.read_csv('C:/Users/45527/Downloads/exoplanet_dataset.csv')
 
-
+print("Data Overview:")
 print(df.head())
 print(df.info())
 print(df.describe())
-df.hist(bins=50, figsize=(20,15))
-plt.show()
 
 
-print(df.isnull().sum())
-df.dropna(inplace=True)
+df = df.dropna(axis=1, how='all')
 
 
-selected_features = [
-    'koi_fpflag_nt',
-    'koi_fpflag_ss',
-    'koi_fpflag_co',
-    'koi_fpflag_ec',
-    'koi_period',
-    'koi_time0bk',
-    'koi_impact',
-    'koi_duration',
-    'koi_depth',
-    'koi_prad',
-    'koi_teq',
-    'koi_insol',
-    'koi_model_snr',
-    'koi_steff',
-    'koi_slogg',
-    'koi_srad'
-]
-X = df[selected_features]
+imputer = SimpleImputer(strategy="mean")
+numeric_columns = df.select_dtypes(include=[np.number]).columns
+df[numeric_columns] = imputer.fit_transform(df[numeric_columns])
+
+
 le = LabelEncoder()
-y = le.fit_transform(df['koi_disposition'])
+df['koi_disposition'] = le.fit_transform(df['koi_disposition'])
 
 
+X = df.select_dtypes(include=[np.number]).drop(['koi_disposition'], axis=1)
+y = df['koi_disposition']
 
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 
+clf = RandomForestClassifier(random_state=42)
+pipeline = Pipeline([('scaler', StandardScaler()), ('classifier', clf)])
+pipeline.fit(X_train, y_train)
 
-# 检查数据集中是否有足够的样本以进行划分
-if len(df) > 0:
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, shuffle=True, stratify=y)
-    # 检查训练集和测试集的样本数量
-    print("训练集样本数:", len(X_train))
-    print("测试集样本数:", len(X_test))
-else:
-    print("数据集中没有足够的样本来进行划分，请检查数据集或数据加载步骤。")
+
+scores = cross_val_score(pipeline, X_train, y_train, cv=5, scoring='accuracy')
+print(f'Mean Cross Validation Score: {scores.mean()}')
+
+
+y_pred = pipeline.predict(X_test)
+print("Accuracy Score: ", accuracy_score(y_test, y_pred))
+print("Classification Report: \n", classification_report(y_test, y_pred))
+print("Confusion Matrix: \n", confusion_matrix(y_test, y_pred))
+
+
+feature_importances = clf.feature_importances_
+features = list(X.columns)
+feature_importance_df = pd.DataFrame({'Features': features, 'Importance': feature_importances})
+feature_importance_df = feature_importance_df.sort_values(by='Importance', ascending=False)
+print("Feature Importances: \n", feature_importance_df)
+
+
+selected_features = ['koi_fpflag_nt', 'koi_fpflag_ss', 'koi_period']  # 替换为你的实际特征列名
+df[selected_features].hist(bins=30, figsize=(10, 7))
+plt.suptitle('Histograms of Selected Features')
+
+
+correlation_matrix = df[selected_features].corr()
+plt.figure(figsize=(10, 7))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', linewidths=.5)
+plt.title('Correlation Heatmap of Selected Features')
+plt.show()
